7721b52388dbeb4416af770c80e1534ed889c2e0
davidhsing <davidhsing@163.com>
2025-01-17 13:58:49 +0800

    Add HttpSessionIdledEvent

---
+++ src/main/java/com/yookue/commonplexus/springutil/security/event/HttpSessionIdledEvent.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016 Yookue Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yookue.commonplexus.springutil.security.event;
+
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.session.AbstractSessionEvent;
+import com.yookue.commonplexus.javaseutil.util.ObjectUtilsWraps;
+
+
+@SuppressWarnings("unused")
+public class HttpSessionIdledEvent extends AbstractSessionEvent {
+    public HttpSessionIdledEvent(@Nonnull HttpSession session) {
+        super(session);
+    }
+
+    public HttpSession getSession() {
+        return ObjectUtilsWraps.castAs(super.getSource(), HttpSession.class);
+    }
+}
