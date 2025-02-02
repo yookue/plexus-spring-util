21328cfc21fe8e93553e866b6b19b23b7f0ce03f
davidhsing <davidhsing@163.com>
2025-02-02 23:09:50 +0800

    Add CaptchaTriggerType

---
+++ src/main/java/com/yookue/commonplexus/springutil/enumeration/CaptchaTriggerType.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Yookue Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yookue.commonplexus.springutil.enumeration;
+
+
+import com.yookue.commonplexus.javaseutil.support.ValueEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+
+/**
+ * Enumerations of captcha trigger types
+ *
+ * @author David Hsing
+ */
+@AllArgsConstructor
+@Getter
+@ToString
+@SuppressWarnings("unused")
+public enum CaptchaTriggerType implements ValueEnum<String> {
+    IMAGE("image-captcha"),    // $NON-NLS-1$
+    MOBILE("mobile-captcha"),    // $NON-NLS-1$
+    SLIDER("slider-captcha");    // $NON-NLS-1$
+
+    private final String value;
+}
