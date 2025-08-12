1e400d09abaafe3a93e54a979f8290ef17e49fad
David Hsing <davidhsing@163.com>
2025-08-12 16:41:43 +0800

    Add JwtAuthenticationDistributedEvent

--- src/main/java/com/yookue/commonplexus/springutil/security/event/JwtAuthenticationDistributedEvent.java
+++ src/main/java/com/yookue/commonplexus/springutil/security/event/JwtAuthenticationDistributedEvent.java
@@ -25,7 +25,7 @@ import lombok.Getter;
 
 
 /**
- * Event when the jwt authentication been created
+ * Event when the jwt authentication been distributed
  *
  * @author David Hsing
  *
@@ -33,8 +33,8 @@ import lombok.Getter;
  */
 @Getter
 @SuppressWarnings("unused")
-public class JwtAuthenticationCreatedEvent extends ApplicationEvent {
-    public JwtAuthenticationCreatedEvent(@Nonnull Authentication authentication) {
+public class JwtAuthenticationDistributedEvent extends ApplicationEvent {
+    public JwtAuthenticationDistributedEvent(@Nonnull Authentication authentication) {
         super(authentication);
     }
 
