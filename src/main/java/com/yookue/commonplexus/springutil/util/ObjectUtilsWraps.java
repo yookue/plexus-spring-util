487a2c22ee8d3508b934abdc85004e345103a063
David Hsing <davidhsing@163.com>
2025-08-25 16:43:10 +0800

    Update ObjectUtilsWraps

--- src/main/java/com/yookue/commonplexus/springutil/util/ObjectUtilsWraps.java
+++ src/main/java/com/yookue/commonplexus/springutil/util/ObjectUtilsWraps.java
@@ -18,6 +18,8 @@ package com.yookue.commonplexus.springutil.util;
 
 
 import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import jakarta.annotation.Nullable;
 import org.springframework.util.ObjectUtils;
 
@@ -60,5 +62,99 @@ public abstract class ObjectUtilsWraps {
     @Nullable
     public static String getIdentityHexString(@Nullable Object source) {
         return (source == null) ? null : ObjectUtils.getIdentityHexString(source);
+    }
+
+    public static <T> void ifEmpty(@Nullable T source, @Nullable Consumer<? super T> action) {
+        ifEmptyOrElse(source, action, null);
+    }
+
+    public static <T> void ifEmpty(@Nullable T source, @Nullable Runnable action) {
+        ifEmptyOrElse(source, action, null);
+    }
+
+    public static <T> void ifEmptyOrElse(@Nullable T source, @Nullable Consumer<? super T> absentAction, @Nullable Consumer<? super T> presentAction) {
+        if (ObjectUtils.isEmpty(source)) {
+            if (absentAction != null) {
+                absentAction.accept(source);
+            }
+        } else {
+            if (presentAction != null) {
+                presentAction.accept(source);
+            }
+        }
+    }
+
+    public static <T> void ifEmptyOrElse(@Nullable T source, @Nullable Runnable absentAction, @Nullable Runnable presentAction) {
+        if (ObjectUtils.isEmpty(source)) {
+            if (absentAction != null) {
+                absentAction.run();
+            }
+        } else {
+            if (presentAction != null) {
+                presentAction.run();
+            }
+        }
+    }
+
+    public static void ifNull(@Nullable Object source, @Nullable Runnable action) {
+        if (source == null && action != null) {
+            action.run();
+        }
+    }
+
+    public static <T> void ifNullOrElse(@Nullable T source, @Nullable Runnable absentAction, @Nullable Consumer<? super T> presentAction) {
+        if (source == null) {
+            if (absentAction != null) {
+                absentAction.run();
+            }
+        } else {
+            if (presentAction != null) {
+                presentAction.accept(source);
+            }
+        }
+    }
+
+    public static <T> void ifNullOrElse(@Nullable T source, @Nullable Runnable absentAction, @Nullable Runnable presentAction) {
+        if (source == null) {
+            if (absentAction != null) {
+                absentAction.run();
+            }
+        } else {
+            if (presentAction != null) {
+                presentAction.run();
+            }
+        }
+    }
+
+    public static <T> void ifNotEmpty(@Nullable T source, @Nullable Consumer<? super T> action) {
+        ifNotEmpty(source, action, null);
+    }
+
+    public static <T> void ifNotEmpty(@Nullable T source, @Nullable Consumer<? super T> action, @Nullable Predicate<? super T> filter) {
+        if (!ObjectUtils.isEmpty(source) && action != null && (filter == null || filter.test(source))) {
+            action.accept(source);
+        }
+    }
+
+    public static <T> void ifNotEmpty(@Nullable T source, @Nullable Runnable action) {
+        if (!ObjectUtils.isEmpty(source) && action != null) {
+            action.run();
+        }
+    }
+
+    public static <T> void ifNotNull(@Nullable T source, @Nullable Consumer<? super T> action) {
+        ifNotNull(source, action, null);
+    }
+
+    public static <T> void ifNotNull(@Nullable T source, @Nullable Consumer<? super T> action, @Nullable Predicate<? super T> filter) {
+        if (source != null && action != null && (filter == null || filter.test(source))) {
+            action.accept(source);
+        }
+    }
+
+    public static <T> void ifNotNull(@Nullable T source, @Nullable Runnable action) {
+        if (source != null && action != null) {
+            action.run();
+        }
     }
 }
